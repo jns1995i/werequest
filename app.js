db6002666185db93b2bd4c0422a105e1d5e7898a
jns1995i <johnniebre1995@gmail.com>
2025-09-30 10:21:19 +0800

    cloudinary

--- app.js
+++ app.js
@@ -86,6 +86,12 @@ const requireAuth = (req, res, next) => {
     next();
 };
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
